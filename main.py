0ef7b37b9bea1cc6028f3bda19c541506f589672
Shrirang Elkunchwar <127132610+Shrirangop@users.noreply.github.com>
2025-08-07 04:27:42 +0530

    Updated port logic

--- main.py
+++ main.py
@@ -56,4 +56,5 @@ async def health_check():
     return {"status": "healthy"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=os.getenv("PORT", 8000), log_level="info")
+
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), log_level="info")
